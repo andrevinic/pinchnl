5b3c496ed8163505c2f2a183ef7879037a6dc817
Andre Vinicius <andrevinicius>
2021-03-03 22:18:38 -0300

    Adjusting xcodegen and first steps

---
+++ PinchNL/PinchNL-Core/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinchNL
+//
+//  Created by Andre Nogueira on 03/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
